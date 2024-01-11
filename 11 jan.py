4179ac2c4430edbfba4daa2c07fea0deff483351
kalyanikaradi <154000642+kalyanikaradi@users.noreply.github.com>
2024-01-11 22:54:38 +0530

    Add files via upload

---
+++ 11 jan.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+np.ones(10)
+
+
+# In[4]:
+
+
+np.ones((4,3))
+
+
+# In[5]:
+
+
+#linspace
+np.eye(3)
+
+
+# In[18]:
+
+
+lspace=np.linspace(4,20,3)
+lspace
+
+
+# In[19]:
+
+
+emp=np.empty((2,3))
+emp
+
+
+# In[6]:
+
+
+np.eye(5,2)
+
+
+# In[8]:
+
+
+big_one=np.ones((3,4))
+print(big_one)
+
+
+# In[9]:
+
+
+big_one*3
+
+
+# In[10]:
+
+
+big_one*4-2
+
+
+# In[11]:
+
+
+import numpy as np
+
+
+# In[12]:
+
+
+listarray=np.array([[2,3,4,5],[6,7,8,9],[10,11,12,13]])
+
+
+# In[13]:
+
+
+listarray
+
+
+# In[14]:
+
+
+listarray.size
+
+
+# In[15]:
+
+
+listarray.shape
+
+
+# In[16]:
+
+
+listarray.ndim
+
+
+# In[23]:
+
+
+np.identity(20)
+
+
+# In[ ]:
+
+
+
+
